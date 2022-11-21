2db1efade83998a6e9508e6aace90788c8da74e4
hokageCodes <hashsenju94@gmail.com>
2022-11-21 10:07:53 +0000

    Notification

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NotificationsPage from './components/NotificationsDashboard/NotificationsPage';
+
+function App() {
+  return (
+    <div className="App">
+      <NotificationsPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NotificationsDashboard/CheckboxInput.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function CheckboxInput() {
+    return (
+        <div className="notification-options">
+            <input className="big-input" type="text" placeholder="Accepted" />
+            <input className="switch" type="checkbox" />
+            <input className="big-input" type="text" placeholder="Rejected" />
+            <input className="switch" type="checkbox" />
+            <input className="big-input" type="text" placeholder="Funded" />
+            <input className="switch" type="checkbox" />
+        </div>
+    );
+}
